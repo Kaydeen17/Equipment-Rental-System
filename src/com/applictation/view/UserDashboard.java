379648ef67ee3833e16bd750194d85b92a1e36bb
jorda <jorda@JordanB>
2025-02-15 15:19:41 -0500

    Updated classes with Login page GUI, Multi Threading

---
+++ src/com/applictation/view/UserDashboard.java
@@ -0,0 +1,23 @@
+package com.applictation.view;
+
+import javax.swing.*;
+
+public class UserDashboard extends JFrame {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public UserDashboard() {
+		
+		
+        setTitle("User Dashboard");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        JLabel label = new JLabel("Welcome, User! Limited access.", SwingConstants.CENTER);
+        add(label);
+        
+        
+    }
+}
